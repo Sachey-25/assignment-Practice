e3318f28291afc50972b9854d5389b21a387fcd0
Sachey-25 <42082775+Sachey-25@users.noreply.github.com>
2020-09-30 17:32:43 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+Escape Sequence					“Escaped” Interpretation
+============================================================================================================
+\a						ASCII Bell (BEL) character
+\b						ASCII Backspace (BS) character
+\f						ASCII Formfeed (FF) character
+\n						ASCII Linefeed (LF) character
+\N{<name>}					Character from Unicode database with given <name>
+\r						ASCII Carriage Return (CR) character
+\t						ASCII Horizontal Tab (TAB) character
+\uxxxx						Unicode character with 16-bit hex value xxxx
+\Uxxxxxxxx					Unicode character with 32-bit hex value xxxxxxxx
+\v						ASCII Vertical Tab (VT) character
+\oxx						Character with octal value xx
+\xhh						Character with hex value hh
